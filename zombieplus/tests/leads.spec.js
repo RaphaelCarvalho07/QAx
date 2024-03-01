315921d40f695bb0a57aa115742de3ca34dc0ae0
Raphael Carvalho <raphaelcarvalho@Mkingfisher.com>
2024-03-01 12:57:19 -0300

    Criando seletores para cadastrar o lead

--- zombieplus/tests/leads.spec.js
+++ zombieplus/tests/leads.spec.js
@@ -4,7 +4,17 @@ const { test, expect } = require("@playwright/test");
 test("deve cadastrar um lead na fila de espera", async ({ page }) => {
   await page.goto("http://localhost:3000/");
 
-  await page.getByRole("button", { name: /Aperte o play/});
+  await page.getByRole("button", { name: /Aperte o play/ }).click()
+
+  await expect(page.getByTestId("modal")
+    .getByRole("heading")).toHaveText("Fila de espera")
+
+  await page.getByPlaceholder("Seu nome completo").fill("Koi Targaryen")
+
+  await page.locator("#email").fill("koi@targaryen.com")
+
+  await page.getByTestId('modal')
+    .getByText('Quero entrar na fila!').click()
 
   // await page.waitForTimeout(10000)
 });
