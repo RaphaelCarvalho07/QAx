776a3249013cd5fdc37fc85304a18dfb3df7a976
Raphael Carvalho <raphaelcarvalho@Mkingfisher.com>
2024-03-13 13:08:24 -0300

    refatoração para uso de desestruturação de objetos

--- zombieplus/tests/e2e/movies.spec.js
+++ zombieplus/tests/e2e/movies.spec.js
@@ -1,23 +1,18 @@
-const { test } = require("../support");
+const { test } = require("../support")
 
-const data = require("../support/fixtures/movies.json");
+const data = require("../support/fixtures/movies.json")
 
 const { executeSQL } = require("../support/database")
 
 test("deve poder cadastrar um novo filme", async ({page}) => {
-  const movie = data.create;
+  const movie = data.create
   await executeSQL(`DELETE from movies where title = '${movie.title}'`)
 
-  await page.login.visit();
-  await page.login.submit("admin@zombieplus.com", "pwd123");
-  await page.movies.isLoggedIn();
+  await page.login.visit()
+  await page.login.submit("admin@zombieplus.com", "pwd123")
+  await page.movies.isLoggedIn()
 
-  await page.movies.create(
-    movie.title,
-    movie.overview,
-    movie.company,
-    movie.release_year
-  );
+  await page.movies.create(movie)
 
-  await page.toast.containText("Cadastro realizado com sucesso!");
+  await page.toast.containText("Cadastro realizado com sucesso!")
 });
--- zombieplus/tests/pages/MoviesPage.js
+++ zombieplus/tests/pages/MoviesPage.js
@@ -2,7 +2,7 @@ const { expect } = require("@playwright/test");
 
 export class MoviesPage {
   constructor(page) {
-    this.page = page
+    this.page = page;
   }
 
   async isLoggedIn() {
@@ -10,32 +10,30 @@ export class MoviesPage {
     await expect(this.page).toHaveURL(/.*admin/);
   }
 
-  async create(title, overview, company, release_year) {
-    await this.page.locator('a[href$="register"]').click()
+  async create(movie) {
+    const { title, overview, company, release_year } = movie;
 
-    await this.page.getByLabel("Titulo do filme").type(title)
-    await this.page.getByLabel("Sinopse").type(overview)
+    await this.page.locator('a[href$="register"]').click();
+
+    await this.page.getByLabel("Titulo do filme").type(title);
+    await this.page.getByLabel("Sinopse").type(overview);
 
     await this.page
       .locator("#select_company_id .react-select__indicator")
-      .click()
+      .click();
 
     await this.page
       .locator(".react-select__option")
       .filter({ hasText: company })
-      .click()
+      .click();
 
-      await this.page
-      .locator("#select_year .react-select__indicator")
-      .click()
+    await this.page.locator("#select_year .react-select__indicator").click();
 
     await this.page
       .locator(".react-select__option")
       .filter({ hasText: release_year })
-      .click()
+      .click();
 
-      await this.page.getByRole('button', {name: 'Cadastrar'}).click()
+    await this.page.getByRole("button", { name: "Cadastrar" }).click();
   }
-
-  
 }
