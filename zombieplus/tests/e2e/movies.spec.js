d88ca5dfe48e9c5155bf2abb991ed04fb665f972
Raphael Carvalho <raphaelcarvalho@Mkingfisher.com>
2024-03-16 12:13:37 -0300

    inclusão de cenário de duplicidade

--- zombieplus/tests/e2e/movies.spec.js
+++ zombieplus/tests/e2e/movies.spec.js
@@ -3,14 +3,28 @@ const { test } = require("../support");
 const data = require("../support/fixtures/movies.json");
 const { executeSQL } = require("../support/database");
 
+test.beforeAll(async () => {
+  await executeSQL(`DELETE from movies`);
+})
+
 test("deve poder cadastrar um novo filme", async ({ page }) => {
   const movie = data.create;
-  await executeSQL(`DELETE from movies where title = '${movie.title}'`);
 
   await page.login.do("admin@zombieplus.com", "pwd123", "Admin");
-  
+
   await page.movies.create(movie);
   await page.toast.containText("Cadastro realizado com sucesso!");
+});
+
+test("não deve cadastrar filme duplicado", async ({ page }) => {
+  const movie = data.duplicate;
+
+  await page.login.do("admin@zombieplus.com", "pwd123", "Admin");
+  await page.movies.create(movie);
+  await page.toast.containText("Cadastro realizado com sucesso!");
+
+  await page.movies.create(movie);
+  await page.toast.containText("Este conteúdo já encontra-se cadastrado no catálogo");
 });
 
 test("não deve cadastrar filme quando os campos obrigatórios não são preenchidos", async ({
