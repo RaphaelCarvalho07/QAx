7422db46658231b78a1d0222138ed7dd0939c4e6
Raphael Carvalho <raphaelcarvalho@Mkingfisher.com>
2024-03-21 16:47:04 -0300

    Inclusão de cenário de exclusão de filme | criação de método na classe Movie para remover filmes pelo nome do filme | configuração do playwright para rodar em simultâneo apenas nas suítes de testes

--- zombieplus/tests/support/actions/Movies.js
+++ zombieplus/tests/support/actions/Movies.js
@@ -49,4 +49,10 @@ export class Movies {
   async alertHaveText(target) {
     await expect(this.page.locator(".alert")).toHaveText(target);
   }
+
+  async remove(title) {
+      // xpath = //td[text()="A Noite dos Mortos-Vivos"]/..//button
+  await this.page.getByRole('row', {name: title}).getByRole('button').click()
+  await this.page.click('.confirm-removal')
+  }
 }
