6fd6a090a58d388efbd8ad14ce4082e34cfde8e7
Raphael Carvalho <raphaelcarvalho@Mkingfisher.com>
2024-03-22 10:33:39 -0300

    inclusão de cenário de buscar por termo zumbi

--- zombieplus/tests/e2e/movies.spec.js
+++ zombieplus/tests/e2e/movies.spec.js
@@ -5,34 +5,37 @@ const { executeSQL } = require("../support/database");
 
 test.beforeAll(async () => {
   await executeSQL(`DELETE from movies`);
-})
+});
 
 test("deve poder cadastrar um novo filme", async ({ page }) => {
   const movie = data.create;
-  
+
   await page.login.do("admin@zombieplus.com", "pwd123", "Admin");
   await page.movies.create(movie);
-  await page.popup.haveText(`O filme '${movie.title}' foi adicionado ao catálogo.`);
+  await page.popup.haveText(
+    `O filme '${movie.title}' foi adicionado ao catálogo.`
+  );
 });
 
-test('deve poder remover um filme', async({page, request}) => {
-  const movie = data.to_remove
+test("deve poder remover um filme", async ({ page, request }) => {
+  const movie = data.to_remove;
   await request.api.postMovie(movie);
 
   await page.login.do("admin@zombieplus.com", "pwd123", "Admin");
   await page.movies.remove(movie.title);
-  await page.popup.haveText('Filme removido com sucesso.')
-})
+  await page.popup.haveText("Filme removido com sucesso.");
+});
 
 test("não deve cadastrar filme duplicado", async ({ page, request }) => {
   const movie = data.duplicate;
   await request.api.postMovie(movie);
-  
+
   await page.login.do("admin@zombieplus.com", "pwd123", "Admin");
   await page.movies.create(movie);
   await page.popup.haveText(
     `O título '${movie.title}' já consta em nosso catálogo.` +
-    ` Por favor, verifique se há necessidade de atualizações ou correções para este item.`);
+      ` Por favor, verifique se há necessidade de atualizações ou correções para este item.`
+  );
 });
 
 test("não deve cadastrar filme quando os campos obrigatórios não são preenchidos", async ({
@@ -43,9 +46,29 @@ test("não deve cadastrar filme quando os campos obrigatórios não são preench
   await page.movies.submit();
 
   await page.movies.alertHaveText([
-    'Campo obrigatório',
-    'Campo obrigatório',
-    'Campo obrigatório',
-    'Campo obrigatório'
+    "Campo obrigatório",
+    "Campo obrigatório",
+    "Campo obrigatório",
+    "Campo obrigatório",
   ]);
+});
+
+test("deve realizar busca pelo termo zumbi", async ({ page, request }) => {
+  // const movies = data.search;
+  const movie1 = data.search1;
+  const movie2 = data.search2;
+  const movie3 = data.search3;
+  const input = "zumbi";
+  const outputs = [movie1.title, movie2.title, movie3.title];
+  // movies.data.forEach(async (m) => {
+  //   await request.api.postMovie(m);
+  // });
+  await request.api.postMovie(movie1);
+  await request.api.postMovie(movie2);
+  await request.api.postMovie(movie3);
+
+  await page.login.do("admin@zombieplus.com", "pwd123", "Admin");
+  await page.movies.search(input);
+
+  await page.movies.tableHave(outputs);
 });
--- zombieplus/tests/support/actions/Movies.js
+++ zombieplus/tests/support/actions/Movies.js
@@ -46,13 +46,27 @@ export class Movies {
     await this.submit();
   }
 
+  async search(target) {
+    await this.page.getByPlaceholder("Busque pelo nome").fill(target);
+
+    await this.page.click('.actions button')
+  }
+
+  async tableHave(content) {
+    const rows = this.page.getByRole("row");
+    await expect(rows).toContainText(content);
+  }
+
   async alertHaveText(target) {
     await expect(this.page.locator(".alert")).toHaveText(target);
   }
 
   async remove(title) {
-      // xpath = //td[text()="A Noite dos Mortos-Vivos"]/..//button
-  await this.page.getByRole('row', {name: title}).getByRole('button').click()
-  await this.page.click('.confirm-removal')
+    // xpath = //td[text()="A Noite dos Mortos-Vivos"]/..//button
+    await this.page
+      .getByRole("row", { name: title })
+      .getByRole("button")
+      .click();
+    await this.page.click(".confirm-removal");
   }
 }
