fe2bd25f652e813e5d74d1a72aad98c806ca0df9
Raphael Carvalho <raphaelcarvalho@Mkingfisher.com>
2024-03-26 11:35:18 -0300

    inclusão de casos de teste para busca por searchTerm | abstração de funções na camada de Components para tornar o reuso coerente

--- zombieplus/tests/support/actions/Movies.js
+++ zombieplus/tests/support/actions/Movies.js
@@ -45,15 +45,4 @@ export class Movies {
 
     await this.submit();
   }
-
-  async search(target) {
-    await this.page.getByPlaceholder("Busque pelo nome").fill(target);
-
-    await this.page.click('.actions button')
-  }
-
-  async tableHave(content) {
-    const rows = this.page.getByRole("row");
-    await expect(rows).toContainText(content);
-  }
 }
